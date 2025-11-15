6c9eb3cf95cf326edef3d70ede9cdb1fc2d1da0b
coderKshiteej <138322650+coderKshiteej@users.noreply.github.com>
2025-11-15 20:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let count = 1;
 
+// Function to add list items
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${count}`;
+    list.appendChild(li);
+    count++;
+  }
+}
+
+// Add 10 items at start
+addItems(10);
+
+// Infinite scroll logic
+window.addEventListener("scroll", () => {
+  const bottomReached =
+    window.innerHeight + window.scrollY >= document.body.offsetHeight;
+
+  if (bottomReached) {
+    addItems(2); // add 2 more items
+  }
+});
